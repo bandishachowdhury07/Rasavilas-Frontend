8175d790b846d65439c4231a1df93357aa31ca5b
Bandisha Chowdhury <bandishachowdhury07@gmail.com>
2025-06-09 23:09:40 +0530

    added code changes for deployment:

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/Rasavilas-Frontend/',
+  base: "/Rasavilas-Frontend",
   build: {
     outDir: 'dist'
   },
